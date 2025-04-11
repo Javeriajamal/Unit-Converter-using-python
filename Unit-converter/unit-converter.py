a1aeaff53cfdd345bd6ec86d50408ebd6638623f
Javeria Jamal <javeriajamal034@gmail.com>
2025-04-11 15:35:17 +0500

    Add files via upload

---
+++ Unit-converter/unit-converter.py
@@ -0,0 +1,45 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="Unit Converter By Javeria Jamal",
+)
+
+
+def convert_units(value, unit_from, unit_to):
+
+    conversions = {
+        "meters_kilometers": 0.001,  
+        "kilometers_meters": 1000,  
+        "grams_kilograms": 0.001,
+        "kilograms_grams": 1000,
+
+    }
+
+    key = f"{unit_from}_{unit_to}"  # Generate a key based on input and output units
+
+    if key in conversions:
+        conversion = conversions[key]
+        # If the conversion is a function (e.g., temperature conversion), call it
+
+        return (
+            conversion(value) if callable(conversion) else value * conversion
+        )  # Otherwise, multiply by the conversion factor
+    
+    else:
+        return "Conversion not supported"  # Return message if conversion is not defined
+
+st.title("Simple Unit Converter") 
+
+value = st.number_input("Enter value:", min_value=1.0, step=1.0)
+
+
+unit_from = st.selectbox("Convert from:", ["meters", "kilometers", "grams", "kilograms"])
+
+unit_to = st.selectbox("Convert to:", ["meters", "kilometers", "grams", "kilograms"])
+
+if st.button("Convert"):
+    result = convert_units(value, unit_from, unit_to)
+    st.write(f"Converted Value: {result}")  
+
+
+
